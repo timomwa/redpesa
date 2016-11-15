e515852fbfe16773a49297d39c4d5eb22f505be8
Timothy Mwangi <timomwa@gmail.com>
2016-11-15 22:15:39 +0300

    Renamed

---
+++ ejb/src/main/java/co/ke/technovation/dao/MpesaInDAOImpl.java
@@ -0,0 +1,7 @@
+package co.ke.technovation.dao;
+
+import co.ke.technovation.entity.MpesaIn;
+
+public class MpesaInDAOImpl extends GenericDAOImpl<MpesaIn, Long> implements MpesaInDAOI {
+
+}
--- ejb/src/main/java/co/ke/technovation/dao/MpesaInImpl.java
+++
@@ -1,7 +0,0 @@
-package co.ke.technovation.dao;
-
-import co.ke.technovation.entity.MpesaIn;
-
-public class MpesaInImpl extends GenericDAOImpl<MpesaIn, Long> implements MpesaInDAOI {
-
-}
